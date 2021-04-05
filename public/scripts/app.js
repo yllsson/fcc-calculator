af69524b8a0a80b47919ef88aea122c366660eb3
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-04-05 10:07:53 +0100

    first commit from local

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log('hello calculator');
+var app = /*#__PURE__*/React.createElement("div", {
+  className: "app"
+}, /*#__PURE__*/React.createElement("h1", null, "hello world"));
+var appRoot = document.getElementById('app');
+ReactDOM.render(app, appRoot);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+const app = (
+  <div className='app'>
+    <h1>hello world</h1>
+  </div>
+);
+
+console.log('hello calculator');
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(app, appRoot);
