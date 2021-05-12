66fc2013ae247110d0b32338bbf32e8b46a5ef8a
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-05-12 20:43:24 +0100

    implementing sass-loader and node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
